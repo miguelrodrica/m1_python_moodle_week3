3546c902d375f34a820eb6e6ed1b72bca6ab64fa
Miguel Rodriguez <mangelrodca@gmail.com>
2025-11-27 15:51:11 -0500

    Add CSV save and load functionality to menu

--- app.py
+++ app.py
@@ -1,40 +1,34 @@
-from services import menu_inventory
-from services import add_product
-from services import show_inventory
-from services import search_product
-from services import update_product
-from services import remove_product
-from services import stats
+from servicios import *
+from archivos import *
 
 inventory = []
 
 while True:
     menu_choice = menu_inventory()
-
     if menu_choice == 1:
         inventory = add_product()
         continue
-
     elif menu_choice == 2:
         show_inventory(inventory)
         continue
-
     elif menu_choice == 3:
         search_product(inventory)
         continue
-
     elif menu_choice == 4:
         update_product(inventory)
         continue
-
     elif menu_choice == 5:
         remove_product(inventory)
         continue
-
     elif menu_choice == 6:
         stats(inventory)
         continue
-
+    elif menu_choice == 7:
+        save_csv(inventory)
+        continue
+    elif menu_choice == 8:
+        load_csv()
+        continue
     else:
         print("\033[93m¡Bye!\033[0m")
         break
